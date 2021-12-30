aa45f9013bb2a7d74a04ee23686526029dcaa5b0
straightupjac <jdc.jaclyn@gmail.com>
2021-12-30 18:23:37 -0500

    metadata

--- contracts/scripts/generateMetadata.py
+++ contracts/scripts/generateMetadata.py
@@ -1,14 +1,14 @@
 import json
 
-baseURI = "https://gateway.pinata.cloud/ipfs/QmcuDmd571ntKgW1qnMVrD6LwX1MoR9dYSFeH3nMnLbxco/"
+baseURI = "https://gateway.pinata.cloud/ipfs/QmNs9kNhW9CWjFWBsczd5yegvSHgYDB87q2iek6Fy26268/"
 
 def generateMetadata(jsonFolderPath):
     # gift
     for idx in range(1, 25):
       data = {}
       data['name'] = "giftNFT #" + str(idx)
-      data['image'] = baseURI + "some.svg"
-      data['description'] = "Describe your NFT"
+      data['image'] = baseURI
+      data['description'] = "Describe your NFT. NFT mint with merkle whitelist scaffold"
       with open(jsonFolderPath+ str(idx) +'.json', 'w+', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
@@ -16,8 +16,8 @@ def generateMetadata(jsonFolderPath):
     for idx in range(26, 1000):
       data = {}
       data['name'] = "whitelist #" + str(idx)
-      data['image'] = baseURI + "some.svg"
-      data['description'] = "Describe your NFT"
+      data['image'] = baseURI
+      data['description'] = "Describe your NFT. NFT mint with merkle whitelist scaffold"
       with open(jsonFolderPath+ str(idx) +'.json', 'w+', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
@@ -25,8 +25,8 @@ def generateMetadata(jsonFolderPath):
     for idx in range(1001, 6000):
       data = {}
       data['name'] = "public mint #" + str(idx)
-      data['image'] = baseURI + "some.svg"
-      data['description'] = "Describe your NFT"
+      data['image'] = baseURI
+      data['description'] = "Describe your NFT. NFT mint with merkle whitelist scaffold"
       with open(jsonFolderPath+ str(idx) +'.json', 'w+', encoding='utf-8') as jsonf:
         jsonf.write(json.dumps(data, indent=4))
 
